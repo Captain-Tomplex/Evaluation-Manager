7ddc077df0125451ab27bcb3a634345166fafed4
tanic21-foi <tanic21@student.foi.hr>
2023-05-22 23:35:28 +0200

    Implementirana klasa Trosak

---
+++ Software/Shparfin/Shparfin/Models/Trosak.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shparfin.Models
+{
+    public class Trosak
+    {
+        public int Id { get; set; }
+
+        public string Kategorija { get; set; }
+
+        public string Komentar {get; set; }
+
+        public int Iznos { get; set; }
+
+        public DateTime Datum { get; set; }
+    }
+}
