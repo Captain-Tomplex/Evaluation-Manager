a324d30bd295bb5298d5258f897df7ad8c111f86
tanic21-foi <tanic21@student.foi.hr>
2023-05-23 00:27:36 +0200

    Kreiran TrosakRepository

---
+++ Software/Shparfin/Shparfin/Repositories/TrosakRepository.cs
@@ -0,0 +1,52 @@
+﻿using DBLayer;
+using Shparfin.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shparfin.Repositories
+{
+    public class TrosakRepository
+    {
+
+        public static List<Trosak> GetTroskove()
+        {
+            List<Trosak> troskovi = new List<Trosak>();
+            string sql = "SELECT * FROM Students";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Trosak trosak = CreateObject(reader);
+                troskovi.Add(trosak);
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return troskovi;
+        }
+
+        public static Trosak CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string kategorija = reader["Kategorija"].ToString();
+            string komentar = reader["Komentar"].ToString();
+            int iznos = int.Parse(reader["Iznos"].ToString());
+            DateTime datum = DateTime.Parse(reader["Datum"].ToString());
+            
+            var trosak = new Trosak
+            {
+                Id = id,
+                Kategorija = kategorija,
+                Komentar = komentar,
+                Iznos = iznos,
+                Datum = datum
+            };
+            return trosak;
+        }
+
+
+    }
+}
